9b4577e0b8261fd29a1f8015f43fa210d7660696
e550226 <joel.wuethrich2@sbb.ch>
2024-12-14 21:00:04 +0100

    Initialize Repo

---
+++ src/main/java/ch/bonapp/bonapi/BonApiApplication.java
@@ -0,0 +1,13 @@
+package ch.bonapp.bonapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BonApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BonApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ch/bonapp/bonapi/BonApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.bonapp.bonapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BonApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
