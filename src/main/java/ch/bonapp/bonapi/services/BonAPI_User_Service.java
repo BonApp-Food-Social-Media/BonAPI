7ef5514c10644e570025ef2260e33a4fd5134da4
e550226 <joel.wuethrich2@sbb.ch>
2024-12-15 00:16:52 +0100

    started implementing followers

--- src/main/java/ch/bonapp/bonapi/services/BonAPI_User_Service.java
+++ src/main/java/ch/bonapp/bonapi/services/BonAPI_User_Service.java
@@ -1,29 +1,31 @@
 package ch.bonapp.bonapi.services;
 
 import ch.bonapp.bonapi.entities.both.User;
+import ch.bonapp.bonapi.entities.both.UserFollowing;
+import ch.bonapp.bonapi.repositories.BonAPI_UserFollowing_Repository;
 import ch.bonapp.bonapi.repositories.BonAPI_User_Repository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 /**
  * Service class for managing User entities.
- * This class provides methods for CRUD operations and custom queries related to Users.
+ * Provides methods for CRUD operations, managing followers, and custom queries related to Users.
  */
 @Service
 public class BonAPI_User_Service {
 
-    /**
-     * Repository for User entities.
-     * Used for accessing the database.
-     */
     @Autowired
     private BonAPI_User_Repository userRepository;
 
+    @Autowired
+    private BonAPI_UserFollowing_Repository userFollowingRepository;
+
     /**
      * Saves or updates a User entity in the database.
-     *
      * @param user The User entity to be saved or updated.
      * @return The saved or updated User entity.
      * @throws IllegalArgumentException if the User or username is null.
@@ -37,7 +39,6 @@ public class BonAPI_User_Service {
 
     /**
      * Retrieves all User entities from the database.
-     *
      * @return A list of all User entities.
      */
     public List<User> getAllUsers() {
@@ -46,7 +47,6 @@ public class BonAPI_User_Service {
 
     /**
      * Retrieves a User entity by its ID.
-     *
      * @param id The ID of the User entity.
      * @return The User entity if found.
      * @throws IllegalArgumentException if the ID is null or empty.
@@ -62,7 +62,6 @@ public class BonAPI_User_Service {
 
     /**
      * Deletes a User entity by its ID.
-     *
      * @param id The ID of the User entity to delete.
      * @throws IllegalArgumentException if the ID is null or empty.
      * @throws RuntimeException if the User with the given ID does not exist.
@@ -79,7 +78,6 @@ public class BonAPI_User_Service {
 
     /**
      * Retrieves a User entity by its username.
-     *
      * @param username The username of the User entity.
      * @return The User entity if found.
      * @throws IllegalArgumentException if the username is null or empty.
@@ -90,5 +88,55 @@ public class BonAPI_User_Service {
             throw new IllegalArgumentException("Username cannot be null or empty.");
         }
         return userRepository.findByUsername(username);
+    }
+
+    /**
+     * Adds a follower relationship between two users.
+     * @param followerId The ID of the user who is following.
+     * @param followedId The ID of the user being followed.
+     */
+    public void addFollower(String followerId, String followedId) {
+        if (followerId.equals(followedId)) {
+            throw new IllegalArgumentException("Users cannot follow themselves.");
+        }
+        UserFollowing newFollow = new UserFollowing(followerId, followedId);
+        userFollowingRepository.save(newFollow);
+    }
+
+    /**
+     * Removes a follower relationship between two users.
+     * @param followerId The ID of the user who is unfollowing.
+     * @param followedId The ID of the user being unfollowed.
+     */
+    public void removeFollower(String followerId, String followedId) {
+        userFollowingRepository.deleteByFollowerIdAndFollowedId(followerId, followedId);
+    }
+
+    /**
+     * Retrieves all followers of a specific user.
+     * @param userId The ID of the user whose followers are being queried.
+     * @return A list of Users who are followers of the specified user.
+     */
+    public List<User> getFollowers(String userId) {
+        List<UserFollowing> followers = userFollowingRepository.findByFollowedId(userId);
+        List<User> followerUsers = followers.stream()
+                .map(follower -> userRepository.findById(follower.getFollowerId()).orElse(null))
+                .filter(Objects::nonNull)
+                .collect(Collectors.toList());
+        return followerUsers;
+    }
+
+    /**
+     * Retrieves all users that a specific user is following.
+     * @param userId The ID of the user whose followings are being queried.
+     * @return A list of Users that the specified user is following.
+     */
+    public List<User> getFollowings(String userId) {
+        List<UserFollowing> followings = userFollowingRepository.findByFollowerId(userId);
+        List<User> followingUsers = followings.stream()
+                .map(following -> userRepository.findById(following.getFollowedId()).orElse(null))
+                .filter(Objects::nonNull)
+                .collect(Collectors.toList());
+        return followingUsers;
     }
 }
