f5389a1cca678b2c27d55247188f109d894f91c7
GurkeSchaleSammler <148222403+GurkeSchaleSammler@users.noreply.github.com>
2025-02-27 21:24:10 +0100

    Update BonAPI_Culinarystyle_Controller.java

--- src/main/java/ch/bonapp/bonapi/controllers/BonAPI_Culinarystyle_Controller.java
+++ src/main/java/ch/bonapp/bonapi/controllers/BonAPI_Culinarystyle_Controller.java
@@ -13,7 +13,7 @@ import java.util.Optional;
  * Provides endpoints for CRUD operations.
  */
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "*")
 @RequestMapping("/culinarystyle")
 public class BonAPI_Culinarystyle_Controller {
 
