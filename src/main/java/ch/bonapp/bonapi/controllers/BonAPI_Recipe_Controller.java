7c48144792fa05b0f7799f2874e07edd93e5976a
GurkeSchaleSammler <148222403+GurkeSchaleSammler@users.noreply.github.com>
2025-02-27 21:24:52 +0100

    Update BonAPI_Recipe_Controller.java

--- src/main/java/ch/bonapp/bonapi/controllers/BonAPI_Recipe_Controller.java
+++ src/main/java/ch/bonapp/bonapi/controllers/BonAPI_Recipe_Controller.java
@@ -13,7 +13,7 @@ import java.util.Optional;
  * Provides endpoints for CRUD operations.
  */
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "*")
 @RequestMapping("/recipe")
 public class BonAPI_Recipe_Controller {
 
